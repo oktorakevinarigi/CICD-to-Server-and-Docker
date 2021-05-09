65abf355e0668cbd27e06fe39ee0750fae5d13a0
Oktora Kevin Arigi <oktora.kevinarigi@ai.astra.co.id>
2021-05-09 13:37:33 +0700

    latest

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Siap
+          Learn React Final
         </a>
       </header>
     </div>
