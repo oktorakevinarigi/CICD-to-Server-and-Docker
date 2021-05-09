90d0c7aad8e8727264f7c7fbd1e57fd7778b4792
Oktora Kevin Arigi <oktora.kevinarigi@ai.astra.co.id>
2021-05-09 12:24:15 +0700

    test cd

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Erik
+          Learn React Test
         </a>
       </header>
     </div>
